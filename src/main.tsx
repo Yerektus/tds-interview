0153bbfedd1367462e7f4b1897264c2f3e8224b9
Yerdos <qop6261@gmail.com>
2026-01-07 21:27:22 +0500

    feat: add query provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './common/styles/index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './router/routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 )
