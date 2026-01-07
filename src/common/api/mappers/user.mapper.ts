6bf6bdce2c5bffd2e458765dd18efd81c336c655
Yerdos <qop6261@gmail.com>
2026-01-07 21:26:25 +0500

    feat: add users request

---
+++ src/common/api/mappers/user.mapper.ts
@@ -0,0 +1,11 @@
+import type { User } from "@/common/entities/user";
+import type { UserResponse } from "../responses/user.reponse";
+
+export function mapUserResponseToUser(userResponse: UserResponse): User {
+  return {
+    id: userResponse.id,
+    firstname: userResponse.firstName,
+    lastname: userResponse.lastName,
+    email: userResponse.email,
+  };
+}
---
+++ src/common/api/requests/fetch-users.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import type { UserResponse } from "../responses/user.reponse";
+import { mapUserResponseToUser } from "../mappers/user.mapper";
+import type { User } from "@/common/entities/user";
+
+export async function fetchUsers(): Promise<User[]> {
+  const response = await axios.get<{
+    users: UserResponse[]
+  }>(
+    "https://dummyjson.com/users"
+  );
+
+  console.log(response)
+
+  return response.data.users.map((userResponse) => mapUserResponseToUser(userResponse));
+}
